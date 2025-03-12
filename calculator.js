7dcb033bc8228df5f0131d8913bc723d98d3b3af
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-03-11 22:31:13 -0700

    add more calculator functions

--- calculator.js
+++ calculator.js
@@ -5,3 +5,52 @@ let add = (...numbers) => {
     });
     return sum;
 };
+
+let subtract = (...numbers) => {
+    let difference = numbers[0];
+    numbers.splice(0, 1);
+    numbers.forEach(num => {
+        difference -= num;
+    });
+    return difference;
+};
+
+let multiply = (...numbers) => {
+    let product = 1;
+    numbers.forEach(num => {
+        product *= num;
+    });
+    return product;
+};
+
+let divide = (...numbers) => {
+    let quotient = numbers[0];
+    numbers.splice(0, 1);
+    numbers.forEach(num => {
+        quotient /= num;
+    });
+    return quotient;
+};
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+let operate = (num1, operator, num2) => {
+    switch(operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+};
